14e15b5e5ab57c0b68a7296202618e3874e62f06
Tina Müller <cpan2@tinita.de>
2019-04-22 16:55:02 +0200

    Add ysh

---
+++ bash/ysh.bash
@@ -0,0 +1,173 @@
+#!bash
+
+_ysh() {
+
+    COMPREPLY=()
+    local program=ysh
+    local cur=${COMP_WORDS[$COMP_CWORD]}
+#    echo "COMP_CWORD:$COMP_CWORD cur:$cur" >>/tmp/comp
+    declare -a FLAGS
+    declare -a OPTIONS
+    declare -a MYWORDS
+
+    local INDEX=`expr $COMP_CWORD - 1`
+    MYWORDS=("${COMP_WORDS[@]:1:$COMP_CWORD}")
+
+    FLAGS=('-M' 'Set the YAML implementation module you wish' '-l' 'Keep a log of all ysh activity in '"\\'"'./ysh.log'"\\'"'. Append mode' '-L' 'Keep a log of all ysh activity in '"\\'"'./ysh.log'"\\'"'. Delete first' '-r' 'Test roundtripping' '-R' 'Same as -r, except that a confirmation message will be printed when the roundtrip succeeds' '-v' 'Print the versions of ysh and the YAML implementation module in use' '-V' 'Like -v, and print the versions of YAML related modules' '--help' 'Show command help' '-h' 'Show command help')
+    OPTIONS=('-i' 'Specify the number of characters to indent each level')
+    __ysh_handle_options_flags
+
+    case ${MYWORDS[$INDEX-1]} in
+      -i)
+        _ysh_compreply "'1'"$'\n'"'2'"$'\n'"'3'"$'\n'"'4'"$'\n'"'5'"$'\n'"'6'"$'\n'"'7'"$'\n'"'8'"
+        return
+      ;;
+
+    esac
+    case $INDEX in
+      0)
+          __comp_current_options || return
+      ;;
+
+
+    *)
+        __comp_current_options || return
+    ;;
+    esac
+
+}
+
+_ysh_compreply() {
+    IFS=$'\n' COMPREPLY=($(compgen -W "$1" -- ${COMP_WORDS[COMP_CWORD]}))
+
+    # http://stackoverflow.com/questions/7267185/bash-autocompletion-add-description-for-possible-completions
+    if [[ ${#COMPREPLY[*]} -eq 1 ]]; then # Only one completion
+        COMPREPLY=( ${COMPREPLY[0]%% -- *} ) # Remove ' -- ' and everything after
+        COMPREPLY=( ${COMPREPLY[0]%% *} ) # Remove trailing spaces
+    fi
+}
+
+
+__ysh_dynamic_comp() {
+    local argname="$1"
+    local arg="$2"
+    local comp name desc cols desclength formatted
+    local max=0
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        if [[ "${#name}" -gt "$max" ]]; then
+            max="${#name}"
+        fi
+    done <<< "$arg"
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        desc="${desc/*$'\t'}"
+        if [[ -n "$desc" && "$desc" != "$name" ]]; then
+            # TODO portable?
+            cols=`tput cols`
+            [[ -z $cols ]] && cols=80
+            desclength=`expr $cols - 4 - $max`
+            formatted=`printf "%-*s -- %-*s" "$max" "$name" "$desclength" "$desc"`
+            comp="$comp$formatted"$'\n'
+        else
+            comp="$comp'$name'"$'\n'
+        fi
+    done <<< "$arg"
+    _ysh_compreply "$comp"
+}
+
+function __ysh_handle_options() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#OPTIONS[@]}; j+=2))
+        do
+            local option="${OPTIONS[$j]}"
+            if [[ "$word" == "$option" ]]; then
+                found=1
+                i=`expr $i + 1`
+                break
+            fi
+        done
+        if [[ -n $found && $i -lt $max ]]; then
+            INDEX=`expr $INDEX - 2`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+function __ysh_handle_flags() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#FLAGS[@]}; j+=2))
+        do
+            local flag="${FLAGS[$j]}"
+            if [[ "$word" == "$flag" ]]; then
+                found=1
+                break
+            fi
+        done
+        if [[ -n $found ]]; then
+            INDEX=`expr $INDEX - 1`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+__ysh_handle_options_flags() {
+    __ysh_handle_options
+    __ysh_handle_flags
+}
+
+__comp_current_options() {
+    local always="$1"
+    if [[ -n $always || ${MYWORDS[$INDEX]} =~ ^- ]]; then
+
+      local options_spec=''
+      local j=
+
+      for ((j=0; j<${#FLAGS[@]}; j+=2))
+      do
+          local name="${FLAGS[$j]}"
+          local desc="${FLAGS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+
+      for ((j=0; j<${#OPTIONS[@]}; j+=2))
+      do
+          local name="${OPTIONS[$j]}"
+          local desc="${OPTIONS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+      __ysh_dynamic_comp 'options' "$options_spec"
+
+      return 1
+    else
+      return 0
+    fi
+}
+
+
+complete -o default -F _ysh ysh
+
