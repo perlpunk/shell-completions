cd12e1b6d1d451923dfa422090f51816a9e744df
Tina Müller <cpan2@tinita.de>
2020-06-05 22:12:23 +0200

    WIP

---
+++ bash/watch.bash
@@ -0,0 +1,183 @@
+#!bash
+
+# Generated with perl module App::Spec v0.013
+
+_watch() {
+
+    COMPREPLY=()
+    local program=watch
+    local cur prev words cword
+    _init_completion -n : || return
+    declare -a FLAGS
+    declare -a OPTIONS
+    declare -a MYWORDS
+
+    local INDEX=`expr $cword - 1`
+    MYWORDS=("${words[@]:1:$cword}")
+
+    FLAGS=('--version' 'output version information and exit' '-v' 'output version information and exit' '--beep' 'beep if command has a non-zero exit' '-b' 'beep if command has a non-zero exit' '--color' 'interpret ANSI color and style sequences' '-c' 'interpret ANSI color and style sequences' '--errexit' 'exit if command has a non-zero exit' '--chgexit' 'exit when output from command changes' '--precise' 'attempt run command in precise intervals' '-p' 'attempt run command in precise intervals' '--no-title' 'turn off header' '-t' 'turn off header' '--help' 'Show command help' '-h' 'Show command help')
+    OPTIONS=('--interval' 'seconds to wait between updates' '-n' 'seconds to wait between updates' '--exec' 'pass command to exec instead of "sh -c"' '-x' 'pass command to exec instead of "sh -c"' '--differences' 'highlight changes between updates' '-d' 'highlight changes between updates')
+    __watch_handle_options_flags
+
+    case ${MYWORDS[$INDEX-1]} in
+      --interval|-n)
+      ;;
+      --exec|-x)
+      ;;
+      --differences|-d)
+        _watch_compreply "cumulative"
+        return
+      ;;
+
+    esac
+    case $INDEX in
+      0)
+          __comp_current_options || return
+      ;;
+
+
+    *)
+        __comp_current_options || return
+    ;;
+    esac
+
+}
+
+_watch_compreply() {
+    local prefix=""
+    cur="$(printf '%q' "$cur")"
+    IFS=$'\n' COMPREPLY=($(compgen -P "$prefix" -W "$*" -- "$cur"))
+    __ltrim_colon_completions "$prefix$cur"
+
+    # http://stackoverflow.com/questions/7267185/bash-autocompletion-add-description-for-possible-completions
+    if [[ ${#COMPREPLY[*]} -eq 1 ]]; then # Only one completion
+        COMPREPLY=( "${COMPREPLY[0]%% -- *}" ) # Remove ' -- ' and everything after
+        COMPREPLY=( "${COMPREPLY[0]%%+( )}" ) # Remove trailing spaces
+    fi
+}
+
+
+__watch_dynamic_comp() {
+    local argname="$1"
+    local arg="$2"
+    local name desc cols desclength formatted
+    local comp=()
+    local max=0
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        if [[ "${#name}" -gt "$max" ]]; then
+            max="${#name}"
+        fi
+    done <<< "$arg"
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        desc="${desc/*$'\t'}"
+        if [[ -n "$desc" && "$desc" != "$name" ]]; then
+            # TODO portable?
+            cols=`tput cols`
+            [[ -z $cols ]] && cols=80
+            desclength=`expr $cols - 4 - $max`
+            formatted=`printf "%-*s -- %-*s" "$max" "$name" "$desclength" "$desc"`
+            comp+=("$formatted")
+        else
+            comp+=("'$name'")
+        fi
+    done <<< "$arg"
+    _watch_compreply ${comp[@]}
+}
+
+function __watch_handle_options() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#OPTIONS[@]}; j+=2))
+        do
+            local option="${OPTIONS[$j]}"
+            if [[ "$word" == "$option" ]]; then
+                found=1
+                i=`expr $i + 1`
+                break
+            fi
+        done
+        if [[ -n $found && $i -lt $max ]]; then
+            INDEX=`expr $INDEX - 2`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+function __watch_handle_flags() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#FLAGS[@]}; j+=2))
+        do
+            local flag="${FLAGS[$j]}"
+            if [[ "$word" == "$flag" ]]; then
+                found=1
+                break
+            fi
+        done
+        if [[ -n $found ]]; then
+            INDEX=`expr $INDEX - 1`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+__watch_handle_options_flags() {
+    __watch_handle_options
+    __watch_handle_flags
+}
+
+__comp_current_options() {
+    local always="$1"
+    if [[ -n $always || ${MYWORDS[$INDEX]} =~ ^- ]]; then
+
+      local options_spec=''
+      local j=
+
+      for ((j=0; j<${#FLAGS[@]}; j+=2))
+      do
+          local name="${FLAGS[$j]}"
+          local desc="${FLAGS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+
+      for ((j=0; j<${#OPTIONS[@]}; j+=2))
+      do
+          local name="${OPTIONS[$j]}"
+          local desc="${OPTIONS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+      __watch_dynamic_comp 'options' "$options_spec"
+
+      return 1
+    else
+      return 0
+    fi
+}
+
+
+complete -o default -F _watch watch
+
