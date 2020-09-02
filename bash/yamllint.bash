6bfe0eff8dcb3a794900a7e671ad7e4046dc3262
Tina Müller <cpan2@tinita.de>
2020-09-02 17:36:30 +0200

    Add yamllint

---
+++ bash/yamllint.bash
@@ -0,0 +1,186 @@
+#!bash
+
+# Generated with perl module App::Spec v0.013
+
+_yamllint() {
+
+    COMPREPLY=()
+    local program=yamllint
+    local cur prev words cword
+    _init_completion -n : || return
+    declare -a FLAGS
+    declare -a OPTIONS
+    declare -a MYWORDS
+
+    local INDEX=`expr $cword - 1`
+    MYWORDS=("${words[@]:1:$cword}")
+
+    FLAGS=('--strict' 'return non-zero exit code on warnings as well as errors' '-s' 'return non-zero exit code on warnings as well as errors' '--version' 'show programs version number and exit' '-v' 'show programs version number and exit' '--help' 'Show command help' '-h' 'Show command help')
+    OPTIONS=('--config-file' 'path to a custom configuration' '-c' 'path to a custom configuration' '--config-data' 'custom configuration as YAML source' '-d' 'custom configuration as YAML source' '--format' 'format for parsing output' '-f' 'format for parsing output')
+    __yamllint_handle_options_flags
+
+    case ${MYWORDS[$INDEX-1]} in
+      --config-file|-c)
+        compopt -o filenames
+        return
+      ;;
+      --config-data|-d)
+      ;;
+      --format|-f)
+        _yamllint_compreply "parsable" "standard" "colored" "auto"
+        return
+      ;;
+
+    esac
+    case $INDEX in
+      0)
+          __comp_current_options || return
+            compopt -o filenames
+      ;;
+
+
+    *)
+        __comp_current_options || return
+    ;;
+    esac
+
+}
+
+_yamllint_compreply() {
+    local prefix=""
+    cur="$(printf '%q' "$cur")"
+    IFS=$'\n' COMPREPLY=($(compgen -P "$prefix" -W "$*" -- "$cur"))
+    __ltrim_colon_completions "$prefix$cur"
+
+    # http://stackoverflow.com/questions/7267185/bash-autocompletion-add-description-for-possible-completions
+    if [[ ${#COMPREPLY[*]} -eq 1 ]]; then # Only one completion
+        COMPREPLY=( "${COMPREPLY[0]%% -- *}" ) # Remove ' -- ' and everything after
+        COMPREPLY=( "${COMPREPLY[0]%%+( )}" ) # Remove trailing spaces
+    fi
+}
+
+
+__yamllint_dynamic_comp() {
+    local argname="$1"
+    local arg="$2"
+    local name desc cols desclength formatted
+    local comp=()
+    local max=0
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        if [[ "${#name}" -gt "$max" ]]; then
+            max="${#name}"
+        fi
+    done <<< "$arg"
+
+    while read -r line; do
+        name="$line"
+        desc="$line"
+        name="${name%$'\t'*}"
+        desc="${desc/*$'\t'}"
+        if [[ -n "$desc" && "$desc" != "$name" ]]; then
+            # TODO portable?
+            cols=`tput cols`
+            [[ -z $cols ]] && cols=80
+            desclength=`expr $cols - 4 - $max`
+            formatted=`printf "%-*s -- %-*s" "$max" "$name" "$desclength" "$desc"`
+            comp+=("$formatted")
+        else
+            comp+=("'$name'")
+        fi
+    done <<< "$arg"
+    _yamllint_compreply ${comp[@]}
+}
+
+function __yamllint_handle_options() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#OPTIONS[@]}; j+=2))
+        do
+            local option="${OPTIONS[$j]}"
+            if [[ "$word" == "$option" ]]; then
+                found=1
+                i=`expr $i + 1`
+                break
+            fi
+        done
+        if [[ -n $found && $i -lt $max ]]; then
+            INDEX=`expr $INDEX - 2`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+function __yamllint_handle_flags() {
+    local i j
+    declare -a copy
+    local last="${MYWORDS[$INDEX]}"
+    local max=`expr ${#MYWORDS[@]} - 1`
+    for ((i=0; i<$max; i++))
+    do
+        local word="${MYWORDS[$i]}"
+        local found=
+        for ((j=0; j<${#FLAGS[@]}; j+=2))
+        do
+            local flag="${FLAGS[$j]}"
+            if [[ "$word" == "$flag" ]]; then
+                found=1
+                break
+            fi
+        done
+        if [[ -n $found ]]; then
+            INDEX=`expr $INDEX - 1`
+        else
+            copy+=("$word")
+        fi
+    done
+    MYWORDS=("${copy[@]}" "$last")
+}
+
+__yamllint_handle_options_flags() {
+    __yamllint_handle_options
+    __yamllint_handle_flags
+}
+
+__comp_current_options() {
+    local always="$1"
+    if [[ -n $always || ${MYWORDS[$INDEX]} =~ ^- ]]; then
+
+      local options_spec=''
+      local j=
+
+      for ((j=0; j<${#FLAGS[@]}; j+=2))
+      do
+          local name="${FLAGS[$j]}"
+          local desc="${FLAGS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+
+      for ((j=0; j<${#OPTIONS[@]}; j+=2))
+      do
+          local name="${OPTIONS[$j]}"
+          local desc="${OPTIONS[$j+1]}"
+          options_spec+="$name"$'\t'"$desc"$'\n'
+      done
+      __yamllint_dynamic_comp 'options' "$options_spec"
+
+      return 1
+    else
+      return 0
+    fi
+}
+
+
+complete -o default -F _yamllint yamllint
+
